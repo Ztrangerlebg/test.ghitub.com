9cabff8218977ea85cae2089ff41cfb835a5036e
Ztrangerlebg <90959480+Ztrangerlebg@users.noreply.github.com>
2021-10-18 17:28:33 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-jQuery( document ).ready(function() { 
+ jQuery( document ).ready(function() { 
   jQuery('.love').click(function(){
     jQuery(this).html('<p class="final">Joyeux anniversaire Helios &nbsp;!</p><img src="https://media.giphy.com/media/feio2yIUMtdqWjRiaF/giphy.gif" width="480" height="270" alt="bon anniv !">');
   });
  });
-
-const app = new Vue({
-  el: '#app'
-});
